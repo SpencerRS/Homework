cb86f982aff1f6c968f5089e378237f27a5b8230
Spencer Richmond <SpizzyRich@Spencers-MacBook-Pro.local>
2016-08-10 09:45:44 -0700

    Morning code challenge to "Overload method" and Adam(has(a(dog())))

---
+++ Labs & Practice/PRACTICE/src/OverloadingMethods.java
@@ -0,0 +1,60 @@
+///**
+// * Created by SpizzyRich on 8/10/16.
+// */
+//public class OverloadingMethods {
+//    public static void main(String[] args) {
+//        String mrOutput = Adam(has(a(dog())));
+//        System.out.println(mrOutput);
+//        String mrAlsoOutput = The(name(of(the(dog(is(also(Adam())))))));
+//        System.out.println(mrAlsoOutput);
+//    }
+//    public static String dog() {
+//        String out = "dog.";
+//        return out;
+//    }
+//    public static String dog(String in){
+//        String out = "dog " + in;
+//        return out;
+//    }
+//    public static String a(String in) {
+//        String out = "a " + in;
+//        return out;
+//    }
+//    public static String has(String in) {
+//        String out = "has " + in;
+//        return out;
+//    }
+//    public static String Adam(String in) {
+//        String out = "Adam " + in;
+//        return out;
+//    }
+//    public static String Adam(){
+//        String out = "Adam.";
+//        return out;
+//    }
+//    public static String The(String in) {
+//        String out = "The " + in;
+//        return out;
+//    }
+//    public static String name(String in) {
+//        String out = "name " + in;
+//        return out;
+//    }
+//    public static String of(String in) {
+//        String out = "of " + in;
+//        return out;
+//    }
+//    public static String the(String in) {
+//        String out = "the " + in;
+//        return out;
+//    }
+//    public static String is(String in) {
+//        String out = "is " + in;
+//        return out;
+//    }
+//    public static String also(String in) {
+//        String out = "also " + in;
+//        return out;
+//    }
+//}
+//
