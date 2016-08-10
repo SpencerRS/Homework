3c0c1fbedc83155522665ea2edccfb8faaf798a2
Spencer Richmond <SpizzyRich@Spencers-MacBook-Pro.local>
2016-08-09 17:26:57 -0700

    Now this does the homework portion of the assignment.

--- SimpleApp1.0/app/src/main/java/com/effoff/spizzyrich/simpleapp10/MainActivity.java
+++ SimpleApp1.0/app/src/main/java/com/effoff/spizzyrich/simpleapp10/MainActivity.java
@@ -3,13 +3,12 @@ package com.effoff.spizzyrich.simpleapp10;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import static android.graphics.Color.BLUE;
-
 public class MainActivity extends AppCompatActivity {
 
 
@@ -35,26 +34,35 @@ public class MainActivity extends AppCompatActivity {
         mrBlueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mrTextOutput.setText("Blue");
                 mrTextOutput.setTextColor(BLUE);
+                Editable input = mrsTextInput.getText();
+                mrTextOutput.setText(input);
             }
         });
         mrRedButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                mrTextOutput.setText("Red");
                 mrTextOutput.setTextColor(Color.RED);
+                Editable input = mrsTextInput.getText();
+                mrTextOutput.setText(input);
             }
         });
         mrGreenButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                mrTextOutput.setText("Green");
                 mrTextOutput.setTextColor(Color.GREEN);
+                Editable input = mrsTextInput.getText();
+                mrTextOutput.setText(input);
             }
         });
 
-
+        mrTextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Editable input = mrsTextInput.getText();
+                mrTextOutput.setText(input);
+            }
+        });
 
     }
 }
