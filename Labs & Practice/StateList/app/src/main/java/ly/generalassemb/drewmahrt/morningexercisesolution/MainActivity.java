3ebe0d5facb9ac57d5eae03f43cc59a81d954295
Spencer Richmond <SpizzyRich@Spencers-MacBook-Pro.local>
2016-08-11 17:22:41 -0700

    Moved some shit inside some other shit.

---
+++ Labs & Practice/StateList/app/src/main/java/ly/generalassemb/drewmahrt/morningexercisesolution/MainActivity.java
@@ -0,0 +1,69 @@
+package ly.generalassemb.drewmahrt.morningexercisesolution;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ListView mListView;
+    ArrayAdapter<String> mArrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.list);
+
+//        Button abbrevButton = (Button) findViewById(R.id.abbreviation_button);
+//        Button fullButton = (Button) findViewById(R.id.fullname_button);
+
+        final ArrayList<String> abbreviationsList = new ArrayList<>(Arrays.asList("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD"));
+        final ArrayList<String> fullNamesList = new ArrayList<>(Arrays.asList("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Main", "Maryland"));
+
+        mArrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, new ArrayList<String>(abbreviationsList));
+        mListView.setAdapter(mArrayAdapter);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, ("Clicked " + position), Toast.LENGTH_SHORT).show();
+                if () {
+                    mArrayAdapter.clear();
+                    mArrayAdapter.addAll(abbreviationsList);
+                    mArrayAdapter.notifyDataSetChanged();
+                } else {
+                    mArrayAdapter.clear();
+                    mArrayAdapter.addAll(fullNamesList);
+                    mArrayAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+//        abbrevButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mArrayAdapter.clear();
+//                mArrayAdapter.addAll(abbreviationsList);
+//                mArrayAdapter.notifyDataSetChanged();
+//            }
+//        });
+//
+//        fullButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mArrayAdapter.clear();
+//                mArrayAdapter.addAll(fullNamesList);
+//                mArrayAdapter.notifyDataSetChanged();
+//            }
+//        });
+    }
+}
