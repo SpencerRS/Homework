5189372e04975ef8c60496d5ff2cba94e50b4ebb
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-08-22 16:46:36 -0700

    Work done by end of day.

--- Labs & Practice/Android Studio/CursorDemo/app/src/main/java/ly/generalassemb/drewmahrt/cursordemo/MainActivity.java
+++ Labs & Practice/Android Studio/CursorDemo/app/src/main/java/ly/generalassemb/drewmahrt/cursordemo/MainActivity.java
@@ -2,11 +2,10 @@ package ly.generalassemb.drewmahrt.cursordemo;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import java.util.Arrays;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,12 +27,34 @@ public class MainActivity extends AppCompatActivity {
         db.setVersion(1);
         db.setLocale(Locale.getDefault());
 
-        String[] countryNames = new String[]{"Cananda","USA","Mexico"};
-        int[] populations = new int[]{35,318,125};
+        String[] countryNames = new String[]{"Cananda", "USA", "Mexico"};
+        int[] populations = new int[]{35, 318, 125};
         db.execSQL("CREATE TABLE IF NOT EXISTS tbl_countries (country_name VARCHAR, population VARCHAR);");
-        for (int i=0; i<countryNames.length;i++) {
-            db.execSQL("INSERT INTO tbl_countries Values ('" + countryNames[i] + "', '"+ populations[i] + "');");
+        for (int i = 0; i < countryNames.length; i++) {
+            db.execSQL("INSERT INTO tbl_countries Values ('" + countryNames[i] + "', '" + populations[i] + "');");
+        }
+        // save info from queary to cursor
+        Cursor cursor = db.query("tbl_countries", new String[]{"country_name"}, "population = ? and country_name = ?", new String[] {"35", "Canada"}, null, null, null);
+
+//        Log.d ("tag", "cursor count: " + cursor.getCount());
+
+//        Log.d("tag", "cursor column count: " + cursor.getColumnCount());
+
+//        Log.d("tag", "cursor columns: " + Arrays.toString(cursor.getColumnName()));
+
+//        cursor.moveToFirst();
+//
+//        while (cursor.isAfterLast() == false){
+//            Log.d("tag","DB result " +cursor.getString(0));
+//            cursor.moveToNext();
+        while (cursor.moveToNext()){
+            Log.d("tag","DB result " +
+                    cursor.getColumnIndex("country_name")
+                    + " " +
+                    cursor.getColumnIndex("population"));
         }
 
+        // close to make sure you save space
+        cursor.close();
     }
 }
